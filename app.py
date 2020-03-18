50f33b9965e5acbdd0571a8b3c188b6545da7ca6
Sabbir Ahmed <fromsabbir@gmail.com>
2020-03-18 07:07:43 +0600

    MVP: agrohi.net

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
 from flask import (
     Flask, render_template, jsonify,
-    Response, request, url_for, send_file
+    redirect, request, url_for, send_file
 )
 import io
 import datalib
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    url = "https://www.worldometers.info/coronavirus/#countries"
-    data = datalib.world_stat(url)
-    return render_template("index.html", name="Corona State", table = data.to_html(index=False), col_names=data.columns.values)
+    df = datalib.world_stat()
+    table_data = df["table"]
+    plot_data = df["plot"]
+    return render_template("index.html", name="Corona State",
+                           table = table_data.to_html(index=False),
+                           col_names=table_data.columns.values,
+                           plot_labels=list(plot_data.index),
+                           plot_values = list(plot_data.values))
+
+
+@app.route('/form-request', methods=["POST"])
+def form_request():
+    data = request.form.get("country")
+    if len(data) > 0:
+        return redirect(f"/{data}")
+    return redirect(url_for("index"))
+
+
+@app.route("/<name>")
+def search_result(name):
+    data = datalib.search_by_country(str(name))
+    if data is None:
+        return render_template("safe-country.html", country_name=name.title())
+    
+    all_data, ser = data
+    country = all_data["Country"]
+    country = country.title()
+    if len(country) < 4:
+        country = country.upper()
+    
+    title = f"Search Result | {country.title()}"
+    all_data.pop("Country")
+    plot_labels = list(all_data.keys())
+    plot_values = list(all_data.values())
+    
+    return render_template("search-result.html", ser=ser,
+                            searched_item=country, name=title,
+                            plot_labels=plot_labels,
+                            plot_values=plot_values)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-0
--- datalib.py
+++ datalib.py
@@ -3,8 +3,6 @@ import locale
 import numpy as np
 import pandas as pd
 import requests as r
-from locale import atof
-locale.setlocale(locale.LC_NUMERIC, '')
 
 BASE_DIR = os.path.dirname(__file__)
 STATIC_DIR = os.path.join(BASE_DIR, "static")
@@ -31,10 +29,13 @@ def fetch_data():
                          "Recovered", "ActivePatients", "CriticalPatients"]
         rename_dict = dict(zip(df.columns.values, new_col_names))
         df = df.rename(columns=rename_dict)
-        df["NewPatients"] = df["NewPatients"].apply(lambda x: x is not None and type(x) is not float and '+' in x and atof(x.split('+')[1]))
+        df["NewPatients"] = df["NewPatients"].apply(lambda x: x is not None and type(x) is not float and '+' in x and int(x.split('+')[1].replace(",", "")))
         df["NewPatients"] = df["NewPatients"].replace(False, 0)
+        df["RecentDeaths"] = df["RecentDeaths"].apply(lambda x: x is not None and type(x) is not float and '+' in x and int(x.split('+')[1].replace(",", "")))
+        df["RecentDeaths"] = df["RecentDeaths"].replace(False, 0)
         df.drop(df[df["Country"].apply(lambda x: "Total" in x)].index.values, inplace=True)
         df.fillna(value=0, inplace=True)
+        df.sort_values("ActivePatients", ascending=1)
         return df
     return None
 
@@ -56,7 +57,9 @@ def world_stat():
     work = pd.DataFrame(data)
     fresh = work[work["ActivePatient"] > 0][ work["RecoveryRate"] > 0]
     sort_by_RR = fresh.sort_values("RecoveryRate", ascending=0)
-    return sort_by_RR
+    plot_data = df.drop('Country', axis=1).sum()
+    plot_data["DeathRate(%)"] = plot_data["Deaths"] / plot_data["Cases"] * 100
+    return {"table": sort_by_RR, "plot": plot_data.sort_values()}
 
 
 def country_list():
@@ -65,12 +68,23 @@ def country_list():
 
 
 def search_by_country(name):
-    country = name
+    df = fetch_data()
+    country = df[df['Country'].apply(lambda x: name.lower() in x.lower())]
+    if len(country) == 0:
+        return None
+    di = country.sort_values("Cases", ascending=0).head(1).to_dict()
+    keys = list(di.keys())
+    values = []
+    for each in di.values():
+        values.append(list(each.values())[0])
+    ser = list(di["Country"].keys())[0] + 1
+    print(ser)
+    return (dict(zip(keys, values)), ser)
     
-    
-
 
 if __name__ == "__main__":
     # update_dataset()
-    print(country_list())    
+    print(country_list())
+    print(world_stat())    
+    print(search_by_country("india"))
 
